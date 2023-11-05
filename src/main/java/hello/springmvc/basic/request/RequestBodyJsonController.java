4db6a584f58a4511b33eab2267866fb9b3404413
bongj9 <lollypark501@naver.com>
2023-11-05 15:38:29 +0900

    refact:requestBody를사용해서축약

--- src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -33,6 +35,22 @@ public class RequestBodyJsonController {
         log.info("Username={},age={}", helloData.getUsername(), helloData.getAge());
 
         response.getWriter().write("ok");
+    }
+    @ResponseBody
+    @PostMapping("/request-body-json-v2")
+    public String requestBodyJsonV2(@RequestBody String messageBody) throws IOException {
+/*        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);*/ /*이걸 @RequestBody String messageBody로 축약시켰따*/
+
+        log.info("messageBody={}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("Username={},age={}", helloData.getUsername(), helloData.getAge());
+
+/*
+        @ResponseBody로 축약
+        response.getWriter().write("ok");
+*/
+        return "OK";
     }
 }
 
