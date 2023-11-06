ed5b0dad66d487d7576515b4470420f361345ae1
bongj9 <lollypark501@naver.com>
2023-11-06 16:18:44 +0900

    refact:ResponseEntity를활용

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -1,6 +1,8 @@
 package hello.springmvc.basic.response;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,9 +13,13 @@ import java.io.IOException;
 @Controller
 public class ResponseBodyController {
 
-    @GetMapping
+    @GetMapping("/response-body-string-v1")
     public void responseBodyV1(HttpServletResponse response) throws IOException {
         response.getWriter().write("ok");
     } //제일 기본적인 방법 response를 받아 getWrite로 하는 방법
 
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String>responseBodyV2() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    } //ResponseEcntity를 활용한 방법
 }
